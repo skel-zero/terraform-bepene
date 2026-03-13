4a88684b1e1bbe9f519050200b249b83af2b9a82
lobatolobato <felipelobato.rj@gmail.com>
2026-03-13 19:15:21 -0300

    test(linux-client-install): add validation tests for the install command on the linux client
    - Verifies that wireguard's config has been properly generated and installed.
    
    Verifies that the cgroup creation and configuration service is being setup correctly, and actually creating and configuring the cgroup.
    
    Checks if the script/client itself has been properly installed.

--- keys/linux_client_template.sh
+++ keys/linux_client_template.sh
@@ -15,9 +15,15 @@ TABLE="1221"
 SCRIPT_PATH=$(realpath "$0")
 INSTALL_PATH=/usr/local/bin/skel0vpn
 
+CGROUP_SERVICE_ID="$CGROUP-cgroup-init.service"
+CGROUP_SERVICE_PATH="/etc/systemd/system/$CGROUP_SERVICE_ID"
+
 # Elevate script
 if [ "$EUID" -ne 0 ] && [ "$1" != "run" ]; then
-   exec sudo "$0" "$@"
+   if [ $DBG == 1 ]; then exec sudo -E "$0" "$@"
+   else
+       exec sudo "$0" "$@"
+   fi
 fi
 
 trim_whitespace() {
@@ -25,80 +31,88 @@ trim_whitespace() {
 }
 
 WireguardConfig() {
-    echo "[Wireguard]:"
-    if [ "$1" == "install"]; then
-        WIREGUARD_CONFIG="
-            [Interface]
-            Address = $ADDRESS
-            DNS = 1.1.1.1
-            Table = off
-            MTU = $MTU
-            PrivateKey = $PRIVATE_KEY
+    WIREGUARD_CONFIG="
+        [Interface]
+        Address = $ADDRESS
+        DNS = 1.1.1.1
+        Table = off
+        MTU = $MTU
+        PrivateKey = $PRIVATE_KEY
 
-            [Peer]
-            PublicKey = $PUBLIC_KEY
-            AllowedIPs = 0.0.0.0/0, ::/0
-            Endpoint = $ENDPOINT:443
-            PersistentKeepalive = 25
-        "
+        [Peer]
+        PublicKey = $PUBLIC_KEY
+        AllowedIPs = 0.0.0.0/0, ::/0
+        Endpoint = $ENDPOINT:443
+        PersistentKeepalive = 25
+    "
+
+    if [ "$1" == "get" ]; then
+        trim_whitespace "$WIREGUARD_CONFIG"
+        exit
+    fi
+
+    echo "[Wireguard]:"
+    if [ "$1" == "install" ]; then
         echo "[#] Installing profile."
         trim_whitespace "$WIREGUARD_CONFIG" > /etc/wireguard/$PROFILE.conf
         echo "[#] Done."
-    elif [ "$1" == "uninstall"]; then
+    elif [ "$1" == "uninstall" ]; then
         echo "[#] Uninstalling profile."
         rm -f /etc/wireguard/$PROFILE.conf
         echo "[#] Done."
     fi
 }
 CGroupInitService() {
-    service_id="$CGROUP-cgroup-init.service"
-    service_path="/etc/systemd/system/$service_id"
+    CGROUP_SERVICE="
+        [Unit]
+        Description=Setup skel0vpn cgroup hierarchy
+        After=local-fs.target
+        Before=network.target
+
+        [Service]
+        Type=oneshot
+        RemainAfterExit=yes
+        # Ensure the directory exists
+        ExecStartPre=/usr/bin/mkdir -p /sys/fs/cgroup/net_cls
+        # Explicitly mount with v1 options, ignoring errors if already mounted
+        ExecStartPre=/usr/bin/sh -c '/usr/bin/mountpoint -q /sys/fs/cgroup/net_cls || /usr/bin/mount -t cgroup -o net_cls net_cls /sys/fs/cgroup/net_cls'
+
+        # Create the specific branch
+        ExecStart=/usr/bin/cgcreate -a $SUDO_USER:$SUDO_USER -t $SUDO_USER:$SUDO_USER -g net_cls:skel0vpn
+        ExecStart=/usr/bin/sh -c 'echo $CLASSID | /usr/bin/tee $CGROUP_PATH/net_cls.classid'
+
+        [Install]
+        WantedBy=multi-user.target
+    "
+
+    if [ "$1" == "get" ]; then
+      trim_whitespace "$CGROUP_SERVICE"
+      exit
+    fi
 
     echo "[CGroup]:"
     if [ "$1" == "install" ]; then
-        CGROUP_SERVICE="
-            [Unit]
-            Description=Setup skel0vpn cgroup hierarchy
-            After=local-fs.target
-            Before=network.target
-
-            [Service]
-            Type=oneshot
-            RemainAfterExit=yes
-            # Ensure the directory exists
-            ExecStartPre=/usr/bin/mkdir -p /sys/fs/cgroup/net_cls
-            # Explicitly mount with v1 options, ignoring errors if already mounted
-            ExecStartPre=/usr/bin/sh -c '/usr/bin/mountpoint -q /sys/fs/cgroup/net_cls || /usr/bin/mount -t cgroup -o net_cls net_cls /sys/fs/cgroup/net_cls'
-
-            # Create the specific branch
-            ExecStart=/usr/bin/cgcreate -a $SUDO_USER:$SUDO_USER -t $SUDO_USER:$SUDO_USER -g net_cls:skel0vpn
-            ExecStart=/usr/bin/sh -c 'echo $CLASSID | /usr/bin/tee $CGROUP_PATH/net_cls.classid'
-
-            [Install]
-            WantedBy=multi-user.target
-        "
-
-        echo "[#]  Installing $service_id..."
-        trim_whitespace "$CGROUP_SERVICE" > $service_path
+        echo "[#]  Installing $CGROUP_SERVICE_ID..."
+        trim_whitespace "$CGROUP_SERVICE" > $CGROUP_SERVICE_PATH
 
         echo "[#]  Enabling service..."
         systemctl daemon-reload
 
-        enable_result=$(trim_whitespace "$(systemctl enable $service_id 2>&1)")
+        enable_result=$(trim_whitespace "$(systemctl enable $CGROUP_SERVICE_ID 2>&1)")
 
         if [ "$enable_result" == "" ]; then
-            systemctl restart $service_id
+            systemctl restart $CGROUP_SERVICE_ID
         else
             echo "[#]  $enable_result"
-            systemctl start $service_id
+            systemctl start $CGROUP_SERVICE_ID
         fi
 
         echo "[#]  Done."
     elif [ "$1" == "uninstall" ]; then
-        echo "[#] Removing $service_id..."
-        systemctl stop $service_id 2>/dev/null
-        systemctl disable $service_id 2>/dev/null
-        rm -f $service_path
+        echo "[#] Removing $CGROUP_SERVICE_ID..."
+        systemctl stop $CGROUP_SERVICE_ID 2>/dev/null
+        systemctl disable $CGROUP_SERVICE_ID 2>/dev/null
+        rm -f $CGROUP_SERVICE_PATH
         systemctl daemon-reload
 
         echo "[#] Deleting $CGROUP cgroup..."
@@ -201,6 +215,17 @@ vpn_status() {
     fi
 }
 
+test_echo_ok() {
+    echo -e "[#] \e[32m$1\e[0m"
+}
+test_echo_fail() {
+    echo -e "[#] \e[31m$1\e[0m"
+}
+
+check_owner() {
+    [[ -z $(find "$1" \( ! -user "$2" -o ! -group "$3" \) -print -quit) ]]
+}
+
 if [ $1 == "install" ]; then
     echo "[#--- Installing VPN ---#]"
 
@@ -266,6 +291,104 @@ elif [ "$1" == "monitor" ]; then
         echo "[VPN] Interface $PROFILE is DOWN"
     fi
 elif [ "$1" == "status" ]; then vpn_status
+elif [ "$1" == "test" ]; then
+    ANSI_GREEN='\e[32m'
+    ANSI_RED='\e[31m'
+    ANSI_RESET='\e[0m'
+
+    verbose=false
+    if [ ! -z "$2" ] && [ "$2" = "-v" ]; then
+        verbose=true
+    fi
+
+    echo "[#--- Tests ---#]"
+    echo "[#command:install]:"
+    # Wireguard config
+    if [ ! -f "/etc/wireguard/$PROFILE.conf" ]; then
+        test_echo_fail "Wireguard profile installation: FAILED"
+        test_echo_fail "  MISSING /etc/wireguard/$PROFILE.conf FILE"
+    elif diff -qB "/etc/wireguard/$PROFILE.conf" <(WireguardConfig get) >/dev/null; then
+        test_echo_ok "Wireguard profile installation: OK"
+    else
+        test_echo_fail "Wireguard profile installation: FAILED"
+        test_echo_fail "  FILE IN /etc/wireguard/$PROFILE.conf DOES NOT MATCH TEMPLATE"
+        if [ $verbose == "true" ]; then
+            diff -B "/etc/wireguard/$PROFILE.conf" <(WireguardConfig get)
+        fi
+    fi
+
+    # CGroup creation and configuration service
+    if [ ! -f "$CGROUP_SERVICE_PATH" ]; then
+        test_echo_fail "CGroup configuration and creation service setup: FAILED"
+        test_echo_fail "  $CGROUP_SERVICE_ID is missing from /etc/systemd/system/"
+    elif ! diff -qB "$CGROUP_SERVICE_PATH" <(CGroupInitService get) >/dev/null; then
+        test_echo_fail "CGroup configuration and creation service setup: FAILED"
+        test_echo_fail "  FILE IN $CGROUP_SERVICE_PATH DOES NOT MATCH TEMPLATE"
+        if [ $verbose == "true" ]; then
+            diff -B "$CGROUP_SERVICE_PATH" <(CGroupInitService get)
+        fi
+    elif ! systemctl is-enabled -q $CGROUP_SERVICE_ID; then
+        test_echo_fail "CGroup configuration and creation service setup: FAILED"
+        test_echo_fail "  $CGROUP_SERVICE_ID is not enabled"
+        test_echo_fail "  try running 'journalctl -u $CGROUP_SERVICE_ID -xe' to see what is wrong"
+    elif ! systemctl is-active -q $CGROUP_SERVICE_ID; then
+        test_echo_fail "CGroup configuration and creation service setup: FAILED"
+        test_echo_fail "  $CGROUP_SERVICE_ID has not been started successfully"
+        test_echo_fail "  try running 'journalctl -u $CGROUP_SERVICE_ID -xe' to see what is wrong"
+        test_echo_fail "  this failure may be related to failures on the tests below, fixing them will probably fix this"
+    else
+        test_echo_ok "CGroup configuration and creation service setup: OK"
+    fi
+
+    # CGroup creation and configuration
+    if [ ! -d "/sys/fs/cgroup/net_cls" ]; then
+        test_echo_fail "CGroup configuration and creation: FAILED"
+        test_echo_fail "  net_cls cgroup is not mounted, probably because the directory was not created"
+        test_echo_fail "  try running systemctl status $CGROUP-cgroup-init.service to see what went wrong"
+    elif ! /usr/bin/mountpoint -q /sys/fs/cgroup/net_cls; then
+        test_echo_fail "CGroup configuration and creation: FAILED"
+        test_echo_fail "  net_cls group is not mounted"
+        test_echo_fail "  try running systemctl status $CGROUP-cgroup-init.service to see what went wrong"
+    elif ! check_owner "$CGROUP_PATH" $SUDO_USER $SUDO_USER; then
+        test_echo_fail "CGroup configuration and creation: FAILED"
+        test_echo_fail "  some or all file/dir gid:pid in $CGROUP_PATH are wrong"
+        test_echo_fail "  they should all be owned by $SUDO_USER:$SUDO_USER"
+        if [$verbose == "true" ]; then
+            test_echo_fail "$(ls -lR "$CGROUP_PATH")"
+        fi
+    elif [ "$(printf '%d' $(cat $CGROUP_PATH/net_cls.classid))" != "$(printf '%d' $CLASSID)" ]; then
+        test_echo_fail "CGroup configuration and creation: FAILED"
+        test_echo_fail "  $CGROUP cgroup's classid is wrong"
+        test_echo_fail "    Got: $(printf '0x%x' $(cat $CGROUP_PATH/net_cls.classid)) | $(printf '%d' $(cat $CGROUP_PATH/net_cls.classid))"
+        test_echo_fail "    Expected: $CLASSID | $(printf '%d' $CLASSID)"
+    else
+      test_echo_ok "CGroup configuration and creation: OK"
+    fi
+
+    # Script installation
+    if [ ! -f "$INSTALL_PATH" ]; then
+        test_echo_fail "Script installation: FAILED"
+        test_echo_fail "  MISSING $INSTALL_PATH FILE"
+    elif [ -f "$SCRIPT_PATH" ] && ! diff -qB "$INSTALL_PATH" "$SCRIPT_PATH" >/dev/null; then
+        test_echo_fail "Script installation: FAILED"
+        test_echo_fail "  FILE IN $INSTALL_PATH DOES NOT MATCH THE ORIGINAL SCRIPT FILE IN $SCRIPT_PATH"
+        if [ $verbose == "true" ]; then
+            diff -qB "$INSTALL_PATH" "$SCRIPT_PATH"
+        fi
+    elif [ ! -x "$INSTALL_PATH" ]; then
+        test_echo_fail "Script installation: FAILED"
+        test_echo_fail "  $INSTALL_PATH is not executable, file permissions are wrong"
+    else
+        test_echo_ok "Script installation: OK"
+    fi
+    # uninstall test
+
+    # up test
+
+    # down test
+
+    # run test
+
 else
-    echo "Usage: $0 {install|uninstall|up|down|run|status|monitor}"
+    echo "Usage: $0 {install|uninstall|up|down|run|status|monitor|test}"
 fi
